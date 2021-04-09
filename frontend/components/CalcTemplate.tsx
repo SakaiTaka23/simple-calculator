31f8d627733a742f44bebe5a1f728cf5874c0ea4
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-09 14:50:25 +0900

    calc全体をまとめるテンプレート作成

---
+++ frontend/components/CalcTemplate.tsx
@@ -0,0 +1,30 @@
+import React, { FC } from 'react';
+import { FormProvider } from 'react-hook-form';
+import { funcMap } from '../hooks/funcMap';
+import useCalc from '../hooks/useCalc';
+import ShowResult from './ShowResult';
+import InputNum1 from './InputNum1';
+import InputNum2 from './InputNum2';
+
+type Prop = {
+  sign: '+' | '-' | '*' | '/' | '%';
+};
+
+const CalcTemplate: FC<Prop> = ({ sign }) => {
+  const { methods, handleChange, Num1, Num2, result } = useCalc(funcMap(sign));
+
+  return (
+    <>
+      <FormProvider {...methods}>
+        <form onChange={methods.handleSubmit(handleChange)}>
+          <InputNum1 />
+          <InputNum2 />
+          <button type='submit'>Submit</button>
+        </form>
+      </FormProvider>
+      <ShowResult Num1={Num1} Num2={Num2} sign={sign} result={result} />
+    </>
+  );
+};
+
+export default CalcTemplate;
