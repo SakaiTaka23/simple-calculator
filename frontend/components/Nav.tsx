30b3909c5ca369431f443b128f05609d4c3bac85
SakaiTaka23 <tt3.sakai@gmail.com>
2021-04-10 09:29:29 +0900

    navbarデザイン

--- frontend/components/Nav.tsx
+++ frontend/components/Nav.tsx
@@ -1,26 +1,34 @@
-import { AppBar } from '@material-ui/core';
+import { AppBar, Grid, Toolbar, Typography } from '@material-ui/core';
 import React from 'react';
 import Link from 'next/link';
 
 const Nav = () => {
   return (
     <AppBar position='static'>
-      <h1>計算アプリケーション</h1>
-      <Link href='/add'>
-        <a>足し算</a>
-      </Link>
-      <Link href='/sub'>
-        <a>引き算</a>
-      </Link>
-      <Link href='/div'>
-        <a>割り算</a>
-      </Link>
-      <Link href='/mult'>
-        <a>掛け算</a>
-      </Link>
-      <Link href='/remain'>
-        <a>余り計算</a>
-      </Link>
+      <Toolbar>
+        <Grid container spacing={10} direction='row' justify='space-around' alignItems='baseline'>
+          <Link href='/'>
+            <a>
+              <Typography variant='h5'>計算アプリケーション</Typography>
+            </a>
+          </Link>
+          <Link href='/add'>
+            <a>足し算</a>
+          </Link>
+          <Link href='/sub'>
+            <a>引き算</a>
+          </Link>
+          <Link href='/div'>
+            <a>割り算</a>
+          </Link>
+          <Link href='/mult'>
+            <a>掛け算</a>
+          </Link>
+          <Link href='/remain'>
+            <a>余り計算</a>
+          </Link>
+        </Grid>
+      </Toolbar>
     </AppBar>
   );
 };
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,22 +1,9 @@
 import React from 'react';
-import Link from 'next/link';
 
 const Home = () => {
   return (
     <>
-      <h1>計算アプリケーション</h1>
-      <Link href='/add'>
-        <a>足し算</a>
-      </Link>
-      <Link href='/sub'>
-        <a>引き算</a>
-      </Link>
-      <Link href='/mult'>
-        <a>割り算</a>
-      </Link>
-      <Link href='/div'>
-        <a>掛け算</a>
-      </Link>
+      <span>I am Landing page</span>
     </>
   );
 };
